95e75904a2fdb6ef71e2b3d711836ca2fe11de7a
Elena Lauren Sparacio <esparacio@elon.edu>
2016-11-01 18:58:51 -0400

    Added blender model and movement

--- Assets/Scripts/BlobPlayer.cs
+++ Assets/Scripts/BlobPlayer.cs
@@ -5,49 +5,33 @@ public class BlobPlayer : MonoBehaviour {
 
 	public float speed = 6.0f;
 	public float gravity = -9.8f;
-	public float vertSpeed;
+	public float vertSpeed = 0;
 	public float jumpSpeed = 100f;
+
 	private CharacterController charController;
-	private Camera mainCamera;
 
-
-	// Use this for initialization
-	void Start () {
-		
+	void Start() {
 		charController = GetComponent<CharacterController>();
-		mainCamera = GetComponent<Camera> ();
-
 	}
 
-	// Update is called once per frame
-	void Update () {
-
-		float oldX = this.transform.position.x;
-		float deltaZ = Input.GetAxis ("Vertical") * speed;
-		Vector3 rotation = new Vector3(0.0f, (Input.GetAxis("Horizontal")), 0.0f);
-
+	void Update() {
+		float deltaX = Input.GetAxis("Horizontal") * speed;
+		float deltaZ = Input.GetAxis("Vertical") * speed;
 
 		if (charController.isGrounded) {
 			if (Input.GetButtonDown ("Jump")) {
 				vertSpeed = jumpSpeed;
-			} else {
-				vertSpeed = 0;
 			}
-		}
-		else{
+		} else {
 			vertSpeed += gravity;
 		}
-		Vector3 movement = new Vector3(oldX, vertSpeed, deltaZ); 
-		movement = Vector3.ClampMagnitude (movement, speed);
 
+
+		Vector3 movement = new Vector3(deltaX, vertSpeed, deltaZ);
 		movement *= Time.deltaTime;
-		movement = transform.TransformDirection (movement);
-		charController.Move (movement);
-		this.transform.Rotate(rotation * speed * Time.deltaTime); 
-
-
+		movement = transform.TransformDirection(movement);
+		charController.Move(movement);
 	}
 
 
 }
-
--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -2,22 +2,68 @@
 using System.Collections;
 
 public class CameraMove : MonoBehaviour {
+	//
+	//    private float speed = 75.0f;
+	//
+	//    // Use this for initialization
+	//    void Start () {
+	//    
+	//    }
 
-	private float speed = 75.0f;
-
-	// Use this for initialization
-	void Start () {
-	
+	public enum RotationAxes
+	{
+		MouseXandY = 0,
+		MouseX = 1,
+		MouseY = 2
 	}
-	
+	// Setting for how the mouse should look
+	public RotationAxes axes = RotationAxes.MouseXandY; //This will be given to the editor X for player and Y for camera. Question: is this a good idea and will it be a permanent change?
+
+	// General settings - sensitivity
+	[SerializeField] private float sensitivityHor = 9.0f;
+	[SerializeField] private float sensitivityVert = 9.0f;
+	// General settings - y rotation constraints
+	[SerializeField] private float maxVert = 45f;
+	[SerializeField] private float minVert = -45f;
+
+	// My rotation value.  We have to keep our
+	// own, since localEulerAngles must be 0-360.
+	// Our variable is kept in range -45 to 45.
+	private float rotationX = 0;
+
 	// Update is called once per frame
 	void Update () {
-	
-		//rotate based on arrow keys 
-		Vector3 vector = new Vector3(0.0f, (Input.GetAxis("Horizontal")), 0.0f);
-		this.transform.Rotate(vector * speed * Time.deltaTime); 
-
-
-
+		float currentXRot = transform.localEulerAngles.x;
+		float currentYRot = transform.localEulerAngles.y;
+		float xRot = getXRot ();
+		float yRot = getYRot ();
+		if (axes == RotationAxes.MouseY)
+			yRot = currentYRot;
+		if (axes == RotationAxes.MouseX)
+			xRot = currentXRot;
+		transform.localEulerAngles = new Vector3 (xRot, yRot, 0);
 	}
+
+	public float getXRot(){
+		float newX = rotationX - Input.GetAxis ("Mouse Y") * sensitivityVert;
+		newX = Mathf.Clamp (newX, minVert, maxVert);
+		rotationX = newX;
+		return newX;
+	}
+
+	public float getYRot(){
+		float newY = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * sensitivityHor;
+		return newY;
+	}
+
+	//    // Update is called once per frame
+	//    void Update () {
+	//    
+	//        //rotate based on arrow keys 
+	//        Vector3 vector = new Vector3(0.0f, (Input.GetAxis("Horizontal")), 0.0f);
+	//        this.transform.Rotate(vector * speed * Time.deltaTime); 
+	//
+	//
+	//
+	//    }
 }
