d18e787e598942aa19ccf942044fe21bd274b29a
Elena Lauren Sparacio <esparacio@elon.edu>
2016-11-02 21:27:37 -0400

    Added assets and grandpa

---
+++ Assets/Scripts/TalkToGrandpa.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TalkToGrandpa : MonoBehaviour {
+
+	public float spawnTime = 0.2f;
+	const float MIN_DIST = 5.0f;
+	GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//InvokeRepeating ("DetectPlayer", spawnTime, spawnTime);
+		player = GameObject.Find ("ActualBlob");
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		DetectPlayer ();
+
+	}
+
+	void DetectPlayer(){
+
+		Vector3 playerLocation = player.transform.position;
+		Vector3 grandpaLocation = this.gameObject.transform.position;
+
+		if (Vector3.Distance(transform.position, playerLocation) <= MIN_DIST){
+			print ("Hello I am grandpa");
+		}
+
+
+	}
+}
